7d656553ec789f3132d1372ea7029255dd450b5d
prasys <pradeesh@prasys.info>
2019-11-12 10:28:27 +1300

    first push

---
+++ randGenerator/randGenerator/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  randGenerator
+//
+//  Created by Pradeesh Parameswaran on 12/11/19.
+//  Copyright © 2019 Pradeesh Parameswaran. All rights reserved.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
